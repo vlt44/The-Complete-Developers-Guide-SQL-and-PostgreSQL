3958a7a6d3d2f85bf86066b6bbc7f26bb790ffa5
vantaylo <vanessalyn.taylor@gmail.com>
2026-06-16 17:23:14 -0500

    compound where clauses

--- Project1/SQLstatements.sql
+++ Project1/SQLstatements.sql
@@ -64,3 +64,7 @@ SELECT name, area FROM cities WHERE area > 4000;
 SELECT name, area FROM cities WHERE area BETWEEN 2000 AND 4000;
 
 SELECT name, area FROM cities WHERE name NOT IN ('Delhi', 'Shanghai');
+
+-- multiple operators
+
+SELECT name, area FROM cities WHERE area NOT IN (3043, 8223) AND name = 'Delhi';
